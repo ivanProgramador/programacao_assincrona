7857311f0e3aa89a799df6677f167808068da1d5
ivan <ivan-guita@hotmail.com>
2024-03-22 21:34:20 -0300

    programação sincrona conceito fim da aula 141

---
+++ Programacao_sincrona.js
@@ -0,0 +1,20 @@
+/*
+A programação sincrona é um jeto mais clássico de desenvolvier códigos
+pois como o nome já diz ela é sincronizada, no caso de uma cdigo sincrono
+o processador executa o codigo uma linha após a outra.
+
+um exemplo em pseudocodigo mostra um cadasro de usuáro ocorre de forma sincrona 
+nesse caso o email nunca será confirmado antes do upload de foto.Uma hash de senha 
+nunca vai ser gerada antes da confirmação de e-mail, porque um codigo sincrono é assim 
+todas as funções tem o momento certo para começar a execução.
+
+A porgramação sincrona é usada em java,php,c# na maioria das vezes é o primeiro tipo 
+de programação que aprendemos a fazer na escola na faculdade ou por conta proria.   
+
+confirmar_email();
+criar_hash_para_senha();
+upload_de_foto();
+gravar_dados();
+
+
+*/
