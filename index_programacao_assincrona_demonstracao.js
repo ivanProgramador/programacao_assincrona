a11f5e64a7eb0443d5ae59d1e08931ba08bed38a
ivan <ivan-guita@hotmail.com>
2024-03-23 22:27:00 -0300

    programação assincrona na prática fimn da aula 144

---
+++ index_programacao_assincrona_demonstracao.js
@@ -0,0 +1,38 @@
+
+
+/*
+ A função enviarEmail() tem um tempo definido de 8 segundos para executar 
+ mas os comandos console.log fora dessa função não tem.
+ Em um cenário de programação sincrona o primeiro console.log 
+ seria executado depois ele ia esperar 8 segundos para executar a função enviarEmail()
+ E só depois os outros console.log seriam exibidos na tela.
+
+ Mas como estamos em um cenario assincrono primiero o porcessador executa
+ as funções que levam menos tempo e depois ele termina de excutar a mais 
+ complexa que exige os 8 segundos, o probela ai e uqe o usuário vai
+ receber que mensagem avisando do envio do e-mail sem ele ter sido enviado ainda 
+ porque não existe uam sequencia pre-definida de execução.
+ 
+ mas mesmo na porgramação assincrona tem opções que podem resolver isso 
+ tornando sincrona a apsrte do código que ineteressa. 
+ 
+ 
+*/
+function enviarEmail(corpo,para){
+    setTimeout(()=>{
+        console.log(`
+        Para: ${para}
+        -----------------------------------
+        ${corpo}
+        -----------------------------------
+        De: Ivan Rodrigues`
+        );
+    },8000);
+}
+
+
+
+console.log('Inicio de evnvio de E-mail');
+enviarEmail('Seja bem vindo','Victor Lima');
+console.log('Seu e-mail foi enviado deve chegar em minutos');
+console.log('Tudo, OK');
